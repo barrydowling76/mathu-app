6cf2cf2de73515975624ec1a55e3be038d7afc33
Barry Dowling <barrydowling76@gmail.com>
2026-03-09 17:19:27 +0000

    Fix Supabase key to use legacy anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://hdfvxjmnodwbbttejasl.supabase.co'
-const supabaseKey = 'sb_publishable_nY8ZaTjSm8VG3gP9evmXLg_ISbP4hCO'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkZnZ4am1ub2R3YmJ0dGVqYXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNzMxMDYsImV4cCI6MjA4ODY0OTEwNn0.2fIq1ycY_3PuWJsQCut3BgswHkEzqswt44SIYh_iIVI'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
